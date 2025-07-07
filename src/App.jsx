8f94c97f462e703d53615a9468a446436ba0d66f
leopalatto20 <A01665462@tec.mx>
2025-07-07 11:02:22 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "@/index.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-black">hola amigos</h1>
+    </div>
+  );
+}
+
+export default App;
