24173bfb7f36d79f7dddc7fd448ea009c97e89e8
leopalatto20 <A01665462@tec.mx>
2025-07-07 16:11:41 -0600

    Agregar imagen de fondo en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-blue-200 text-center p-12">
-      <h1 className="text-4xl font-bold">Impulsando la cultura de Paz</h1>
-      <p className="text-xl mt-4">
+    <header
+      className="bg-[url(/cultura_paz.png)] bg-cover bg-center 
+      bg-no-repeat text-center p-12"
+    >
+      <h1 className="text-4xl font-bold bg-green-600/65 rounded-lg p-3">
+        Impulsando la cultura de Paz
+      </h1>
+      <p className="text-xl mt-4 bg-green-600/65 rounded-lg p-3">
         Fomentando la cultura de paz en el municipio de El Salto, Jalisco.
       </p>
     </header>
--- src/components/Principles.jsx
+++ src/components/Principles.jsx
@@ -1,18 +1,16 @@
-import React from "react";
-
 export default function Principles() {
   const principles = [
-    "Respect for all life",
-    "Non-violence",
-    "Sharing with others",
-    "Listening to understand",
-    "Preserving the planet",
-    "Tolerance and solidarity",
+    "Respeto por la vida",
+    "No violencia",
+    "Compartir con otros",
+    "Escuchar para comprender",
+    "Preservar el planeta",
+    "Tolerancia y solidaridad",
   ];
 
   return (
     <section className="bg-gray-100 p-8">
-      <h2 className="text-3xl font-bold text-center mb-6">Core Principles</h2>
+      <h2 className="text-3xl font-bold text-center mb-6">Principios clave</h2>
       <div className="grid md:grid-cols-3 gap-4 text-center">
         {principles.map((principle, index) => (
           <div key={index} className="bg-white p-6 rounded-lg shadow">
