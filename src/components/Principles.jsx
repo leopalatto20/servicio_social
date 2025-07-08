8e8206b2b9f95e1db7e9dce33f730443171b3ad1
leopalatto20 <A01665462@tec.mx>
2025-07-07 18:25:21 -0600

    Tarjetas de principios

--- src/components/Principles.jsx
+++ src/components/Principles.jsx
@@ -13,9 +13,13 @@ export default function Principles() {
       <h2 className="text-3xl font-bold text-center mb-6 font-poppins">
         Principios clave
       </h2>
-      <div className="grid md:grid-cols-3 gap-4 text-center">
+      <div className="grid md:grid-cols-3 gap-4 text-center ">
         {principles.map((principle, index) => (
-          <div key={index} className="bg-white p-6 rounded-lg shadow">
+          <div
+            key={index}
+            className="bg-white p-6 rounded-lg shadow hover:bg-pink-100 
+            duration-300 hover:scale-110"
+          >
             <h3 className="text-xl font-bold font-lato">{principle}</h3>
           </div>
         ))}
