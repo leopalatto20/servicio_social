d08e166371e78cceac36ff110a601456ec785f4b
leopalatto20 <A01665462@tec.mx>
2025-07-08 11:02:01 -0600

    quitar imagenes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,30 +2,20 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="p-8 bg-white" id="About">
-      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-        <img
-          src="/tec.png"
-          alt="Cultura de Paz"
-          className="w-full h-auto rounded-xl shadow"
-        />
-
-        <div>
-          <h2 className="text-4xl font-poppins font-bold mb-6 text-center md:text-left">
-            ¿Qué es la cultura de paz?
-          </h2>
-          <p className="text-justify md:text-left text-2xl font-lato">
-            La cultura de paz se refiere a un conjunto de valores, actitudes y
-            comportamientos que promueven la resolución pacífica de conflictos,
-            la convivencia armónica y el respeto por los derechos humanos. En un
-            mundo donde la violencia y la intolerancia parecen ser cada vez más
-            comunes, es fundamental entender y fomentar este concepto. Pero,
-            ¿qué implica realmente este enfoque? En términos simples, se trata
-            de construir una sociedad donde se priorice el diálogo, la empatía y
-            la cooperación sobre la agresión y el enfrentamiento.
-          </p>
-        </div>
-      </div>
+    <section className="p-8" id="About">
+      <h2 className="text-4xl font-poppins font-bold text-center mb-6">
+        ¿Qué es la cultura de paz?
+      </h2>
+      <p className="max-w-4xl mx-auto text-center text-2xl font-lato">
+        La cultura de paz se refiere a un conjunto de valores, actitudes y
+        comportamientos que promueven la resolución pacífica de conflictos, la
+        convivencia armónica y el respeto por los derechos humanos. En un mundo
+        donde la violencia y la intolerancia parecen ser cada vez más comunes,
+        es fundamental entender y fomentar este concepto. Pero, ¿qué implica
+        realmente este enfoque? En términos simples, se trata de construir una
+        sociedad donde se priorice el diálogo, la empatía y la cooperación sobre
+        la agresión y el enfrentamiento.
+      </p>
     </section>
   );
 }
